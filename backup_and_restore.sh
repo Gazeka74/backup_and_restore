077132aa885ad2679baa998f5c45dc3e2b13f66a
gazeka74 <gazeka74@gmail.com>
2019-12-20 11:35:27 +0100

    added INVALID_PARAMETER

--- backup_and_restore.sh
+++ backup_and_restore.sh
@@ -4,26 +4,36 @@ WHEREAMI=$(pwd)
 
 WHOAMI=$(whoami)
 
-OPTIONS="Options:
-    -h, --help      This help menu.
-    -r, --restore   Restore the dotfiles to the correct location
-    -b, --backup    Backup the dotfiles and git add/commit/push"
+DESCRIPTOR="
+$0 : a small script to backup and restore dotfiles"
+
+USAGE="
+Usage :
+	$0 [options]
+Options :
+	-h, --help      This help menu
+	-r, --restore   Restore the dotfiles to the correct location
+	-b, --backup    Backup the dotfiles and git add/commit/push"
 
 function print_usage {
-    printf "\n$0  : a small script to backup and restore dotfiles \n\nUsage:\n     $(basename $0) [options]\n\n$OPTIONS\n\n"
+	printf "$DESCRIPTOR \n$USAGE"
 }
 
 while true; do
-    if [ -z "$1" ]; then
-        print_usage ; exit 1
-    fi
-    case "$1" in
-        -h|--help)
-            print_usage ; exit 0 ;;
-        -b|--backup)
-            echo backup; exit 0;;
-        -r|--restore)
-            echo restore; exit 0;;
-    esac
+	if [ -z "$1" ]; then
+		print_usage ; exit 1
+	fi
+	
+	case "$1" in
+	-h|--help)
+        	print_usage ; exit 0 ;;
+	-b|--backup)
+		echo backup; exit 0;;
+	-r|--restore)
+		echo restore; exit 0;;
+	*)	
+		printf "INVALID_PARAMETER\n$USAGE"; exit 1;;
+
+	esac
 done
 
